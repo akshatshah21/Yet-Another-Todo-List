6dde7903966075a16100391e474f1b26c38a70b8
akshatshah21 <akshatshah_21@yahoo.in>
2020-06-23 18:39:54 +0530

    ...

--- index.js
+++ index.js
@@ -30,6 +30,6 @@ if (true || process.env.NODE_ENV === "production") {
 }
 
 const PORT = process.env.PORT | 5000;
-app.listen(PORT, () => {
+app.listen(PORT, "0.0.0.0", () => {
   console.log(`Server has started on ${PORT}`);
 });
